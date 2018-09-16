7215723259d1aff1500c7c0d7a22a1d23a595c03
Faheem Patel <faheem@faheempatel.com>
2018-09-16 14:42:35 +0100

    Setup parcel + preact

---
+++ index.js
@@ -0,0 +1,9 @@
+import { h, render } from 'preact';
+
+render(
+  <div id="foo">
+    <span>Hello, world!</span>
+    <button onClick={e => alert('hi!')}>Click Me</button>
+  </div>,
+  document.body
+);
