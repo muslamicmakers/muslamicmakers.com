24943734a8c8e86033dc6822446fdfbc0522e72e
Faheem Patel <faheem@faheempatel.com>
2018-09-23 00:40:03 +0100

    Change API requests to match backend + add loading spinners

--- components/HomePage/index.jsx
+++ components/HomePage/index.jsx
@@ -1,5 +1,6 @@
 import { h, Component } from 'preact';
-import getData from '../../lib/api';
+import Loader from 'react-loader-spinner';
+import { getData, getEventbrite } from '../../lib/api';
 
 import Nav from '../Nav';
 import Header from '../Header';
@@ -13,12 +14,16 @@ import Footer from '../footer';
 class HomePage extends Component {
   constructor() {
     super();
-    this.state = { data: null };
+    this.state = { data: null, eventbrite: null };
   }
 
   componentDidMount() {
     getData.then(data => {
       this.setState({ data });
+    });
+
+    getEventbrite.then(eventbrite => {
+      this.setState({ eventbrite });
     });
   }
 
@@ -29,14 +34,26 @@ class HomePage extends Component {
           <Nav />
           <Header {...this.state.data.headerData} />
         </header>
-        <EventbriteSection event={this.state.data.eventbriteData} />
+
+        {this.state.eventbrite ? (
+          <EventbriteSection event={this.state.eventbrite} />
+        ) : (
+          <div className="loader">
+            <Loader type="Oval" color="#30AFFF" height="50" width="50" />
+          </div>
+        )}
+
         <AboutSection />
         <VideoSection videos={this.state.data.videoData} />
         <WhoAreWeSection bios={this.state.data.bioData} />
         <ContactSection />
         <Footer />
       </div>
-    ) : null;
+    ) : (
+      <div className="loader">
+        <Loader type="Oval" color="#30AFFF" height="50" width="50" />
+      </div>
+    );
   }
 }
 
