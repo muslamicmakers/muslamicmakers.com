11e8acbe9a3293f197284dcbd96122ee03e1e59b
Faheem Patel <faheem@faheempatel.com>
2019-04-08 16:34:06 +0100

    Add redirect and 404

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import { h, render } from 'preact';
-import { Router } from '@reach/router';
+import { Router, Redirect } from '@reach/router';
 
 import HomePage from './components/HomePage';
 import Manifesto from './components/Manifesto';
 
+const NotFound = () => <div>404 - Sorry, nothing here :(</div>;
+
 const Main = () => (
   <Router>
     <HomePage path="/" />
+    <Redirect from="/manifesto.html" to="/manifesto" noThrow />
     <Manifesto path="/manifesto" />
+    <NotFound default />
   </Router>
 );
 
